384ff110fff766190d7514dc663f9ed3ee0842bf
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-11-08 16:52:32 +0800

    Update cut_adapter.sh

--- cut_adapter.sh
+++ cut_adapter.sh
@@ -8,5 +8,5 @@
 cd $PBS_O_WORKDIR
 
 
-cat file_list.txt|while read id;do /gpfshome/home/Changq/miniconda3/bin/cutadapt -a AGATCGAAGAG \
---quality-base 33 -m 10 -q 20, 20 -o trim_${id} /gpfshome/home/Changq/Project/guoyi/raw_data/$id;done
+cat file_list.txt|while read id;do /home/Changq/miniconda3/bin/cutadapt -a AGATCGAAGAG \
+--quality-base 33 -m 10 -q 20, 20 -o trim_${id} /home/Changq/Project/raw_data/$id;done
